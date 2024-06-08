5f6fd224d61d30766e8c87995192d0581f3fe210
IfeoluwaGIT <idurobello@gmail.com>
2024-06-08 20:29:54 +0100

    updates to the page styling, precisely margin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         <div className='middle-container'>
         <div className="text-container">
       <h1>Hello! My name is Ifeoluwa Duro-Bello</h1>
-      <p>Young Web Developer with a solid foundation in HTML, CSS, JavaScript, and React, eager to expand my skills and tackle new challenges in web development. </p>
+      <p>Young Web Developer with a solid foundation in HTML, CSS, JavaScript, and React,<br></br> eager to expand my skills and tackle new challenges in <br></br>web development. </p>
       </div>
       </div>
       </div>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,19 +4,19 @@ import Project from './Project';
 const ProjectList = () => {
   const projects = [
     {
-      title: 'React Jobs',
+      title: 'React-Jobs',
       description: 'A practice React job website where you can check for React jobs, also built a simple backend where the data was fetched from ',
       link: 'https://github.com/IfeoluwaGIT/react-jobs'
     },
     {
-      title: 'Project 2',
-      description: 'Description for project 2',
-      link: 'http://example.com/project2'
+      title: 'Spotify-by-Iboy',
+      description: 'Spotify-by-Iboy is a dynamic React web application designed to enhance your music experience by allowing you to search the Spotify library, create custom playlists, and save them directly to your Spotify account. Leveraging the power of React components, state management, and the Spotify API, this project showcases advanced skills in front-end development and API integration.',
+      link: 'https://github.com/IfeoluwaGIT/Spotify-by-Ife'
     },
     {
-      title: 'Project 3',
-      description: 'Description for project 3',
-      link: 'http://example.com/project2'
+      title: 'My-Portfolio',
+      description: 'This personal portfolio website, built with React, showcases my skills and projects in a visually engaging and interactive manner. The site features smooth SVG animations and a responsive design, ensuring an optimal viewing experience across all devices. It highlights my expertise in modern web development techniques and my ability to create polished, user-friendly web applications.',
+      link: 'https://github.com/IfeoluwaGIT/Spotify-by-Ife'
     },
     // Add more projects here
   ];
