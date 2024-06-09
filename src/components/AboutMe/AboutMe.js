8251e2df6585932c48fa2bee5d4438741fddac73
IfeoluwaGIT <idurobello@gmail.com>
2024-06-09 10:43:24 +0100

    further changes to meadia queries

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <h2>About Me</h2>
       <div className="content">
         <p>
-        I'm a dynamic professional with a background in law and a passion for technology, merging analytical prowess with creative innovation. With expertise in front-end development, including HTML, CSS, JavaScript, and React, I craft intuitive user experiences that marry legal integrity with cutting-edge design. My journey from law to tech reflects my commitment to continuous learning and adaptability, allowing me to navigate complex challenges with clarity and precision. Beyond work, I find inspiration in sports, family, and music, fostering a balanced lifestyle that fuels my creativity and drive.
+        I'm a dynamic professional with a background in law and a passion for technology, merging analytical prowess with creative innovation. With expertise in front-end development, including HTML, CSS, JavaScript, React and Redux, I craft intuitive user experiences, with cutting-edge design. My journey from law to tech reflects my commitment to continuous learning and adaptability, allowing me to navigate complex challenges with clarity and precision. Beyond work, I find inspiration in sports, family, and music, fostering a balanced lifestyle that fuels my creativity and drive.
         </p>
     
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <img src={iboy} alt="Logo" className="logo" />
         <div className="text-container">
       <h1>Hello! My name is Ifeoluwa Duro-Bello</h1>
-      <p className="description">Young Web Developer with a solid foundation in HTML, CSS, JavaScript, and React, eager to expand my skills and tackle new challenges in web development.</p>
+      <p className="description">Young Web Developer with a solid foundation in HTML, CSS, JavaScript, React and Redux, eager to expand my skills and tackle new challenges in web development.</p>
       </div>
       </div>
     </header>
