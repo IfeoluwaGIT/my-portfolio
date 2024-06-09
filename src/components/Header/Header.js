5e50434fbfb66d5d08dbebdef81231ee6a7b91b7
IfeoluwaGIT <idurobello@gmail.com>
2024-06-09 09:13:04 +0100

    update to media queries header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,11 @@ import './Header.css';
 const Header = () => {
   return (
     <header >
-      <img src={iboy} alt="Logo" className="logo" />
       <div className="header-container">
-        <div className='middle-container'>
+      <img src={iboy} alt="Logo" className="logo" />
         <div className="text-container">
       <h1>Hello! My name is Ifeoluwa Duro-Bello</h1>
       <p>Young Web Developer with a solid foundation in HTML, CSS, JavaScript, and React,<br></br> eager to expand my skills and tackle new challenges in <br></br>web development. </p>
-      </div>
       </div>
       </div>
     </header>
